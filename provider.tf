8043ccfa33178a867c45e0f1cdf8b6a4a7383a76
Ivan Loor <ivanols@hotmail.com>
2022-07-29 13:55:55 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,13 +4,3 @@ provider "aws" {
   #access_key = var.access_key
   #secret_key = var.secret_key
 }
-
-terraform {
-  required_version = ">= 0.12.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
